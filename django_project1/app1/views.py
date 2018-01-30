57cab76a3f222af8cc59927deda1c9e02a26e224
raoqinglin <827396327@qq.com>
2018-01-30 22:55:26 +0800

    添加模板文件和引入bootstrap

--- django_project1/app1/views.py
+++ django_project1/app1/views.py
@@ -1,6 +1,10 @@
 # coding:utf-8
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse(u"第一个app应用123！")
+    # return HttpResponse(u"第一个app应用2222！")
+    
+    # 使用视图模板
+    return render(request, 'index.html')
