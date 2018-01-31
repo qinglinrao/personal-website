3c8be65e2c2c4f11862057560474475ed628efdb
raoqinglin <827396327@qq.com>
2018-01-31 16:04:42 +0800

    更新

--- django_project1/app1/views.py
+++ django_project1/app1/views.py
@@ -7,4 +7,5 @@ def index(request):
     # return HttpResponse(u"第一个app应用2222！")
     
     # 使用视图模板
-    return render(request, 'index.html')
+    string = "模板测试变量"
+    return render(request, 'index.html', {'string': string})
