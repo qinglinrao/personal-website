64acb1ce37f4fcc2e9f330856b8398b389e1b994
raoqinglin <827396327@qq.com>
2018-02-06 23:34:17 +0800

    更新

--- django_project1/app1/models.py
+++ django_project1/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    # 因为mysql不是默认的数据库，所以用到的Field不一样--todo
+    name = models.charField(max_length=30)
+    age = models.IntegerField()
+
